c0eb22e0a57e485e308987eb1570a9deb1ef0a1b
AndreTheViking <andre.masson@norda.com>
2023-05-22 17:24:47 -0400

    feat: snackbar example and theming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.snackbar import Snackbar
+from kivymd.uix.label import MDLabel
 from kivymd.app import MDApp
 
 class ContentNavigationDrawer(MDBoxLayout):
@@ -6,8 +8,19 @@ class ContentNavigationDrawer(MDBoxLayout):
 
 class MainApp(MDApp):
     def testMultipleNamedParams(self, firstName, lastName):
-        # text = self.testMultipleNamedParams(firstName="John", lastName="Smith")
         return "The formatted\nusername: '{} {}'".format(firstName, lastName)
+    
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+
+    def show_info(self):
+        self.root.ids['screen_manager'].current = "about"
+        text = self.testMultipleNamedParams(firstName="John", lastName="Smith")
+        Snackbar(text=text).open()
+    
+    def exit(self):
+        self.stop()
 
 if __name__ == '__main__':
     MainApp().run()
