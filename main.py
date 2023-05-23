2ef5625d2464e9313058b6321556015271544689
AndreTheViking <andre.masson@norda.com>
2023-05-23 07:58:35 -0400

    feat: add dialog example

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.snackbar import Snackbar
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 from kivymd.uix.label import MDLabel
 from kivymd.app import MDApp
 
@@ -7,6 +9,8 @@ class ContentNavigationDrawer(MDBoxLayout):
     pass
 
 class MainApp(MDApp):
+    dialog = None
+
     def testMultipleNamedParams(self, firstName, lastName):
         return "The formatted\nusername: '{} {}'".format(firstName, lastName)
     
@@ -18,6 +22,23 @@ class MainApp(MDApp):
         self.root.ids['screen_manager'].current = "about"
         text = self.testMultipleNamedParams(firstName="John", lastName="Smith")
         Snackbar(text=text).open()
+
+    def send_message(self, text):
+        Snackbar(text=text).open()
+    
+    def more_info(self):
+        title = self.root.appName
+        self.dialog = MDDialog(title=title, text="This is a test dialog", auto_dismiss=True,
+                buttons=[
+                    MDFlatButton(text="OK", theme_text_color="Primary", on_release=lambda _: self.on_dialog_close("OK")),
+                    MDFlatButton(text="Cancel", on_release=lambda _: self.on_dialog_close("Cancel")),
+                ],
+        )
+        self.dialog.open()
+    
+    def on_dialog_close(self, action):
+        self.dialog.dismiss()
+        Snackbar(text=action).open()
     
     def exit(self):
         self.stop()
