4237f6ab162eae17ade6439c98885e94b5d38e83
AndreTheViking <andre.masson@norda.com>
2023-07-16 13:49:47 -0400

    refactor: improved pref service typing and methods

--- libs/utils/preferences_service.py
+++ libs/utils/preferences_service.py
@@ -1,33 +1,33 @@
 from enum import Enum
-import sqlite3
+
 from libs.utils.storage_service import StorageService
 
 Preferences = Enum("Preferences", ["OPEN_AI_KEY", "AI_SYSTEM_INITIAL_CONTEXT", "AI_TEMPERATURE", "THEME_PRIMARY_COLOR", "THEME_STYLE"])
 
 class PreferencesService:
-    def __init__(self):
+    def __init__(self) -> None:
         self.storage_service = StorageService("preferences.db")
         self.storage_service.connect()
-        self.storage_service.create_table("preferences", "key TEXT, value TEXT")
+        self.storage_service.create_table("preferences", ["key TEXT", "value TEXT"])
         self.storage_service.close()
 
-    def get(self, key, default_value = None):
+    def get(self, key, default_value = None) -> str:
         self.storage_service.connect()
-        result = self.storage_service.select("preferences", "value", f'key = "{key}"')
+        result = self.storage_service.select("preferences", ["value"], f'key = "{key}"')
         self.storage_service.close()
         if len(result) == 0:
             return default_value
         else:
             return result[0][0]
 
-    def set(self, key, value = None):
+    def set(self, key, value = None) -> None:
         self.storage_service.connect()
         self.storage_service.delete("preferences", f'key = "{key}"')
         if value is not None:
-            self.storage_service.insert("preferences", "key, value", f'"{key}", "{value}"')
+            self.storage_service.insert("preferences", ["key", "value"], [f'"{key}"', f'"{value}"'])
         self.storage_service.close()
 
-    def delete(self, key):
+    def delete(self, key) -> None:
         self.storage_service.connect()
         self.storage_service.delete("preferences", f'key = "{key}"')
         self.storage_service.close()
--- libs/utils/storage_service.py
+++ libs/utils/storage_service.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+SEP = ", "
+
 class StorageService:
     def __init__(self, db_name):
         self.db_name = db_name
@@ -14,6 +16,7 @@ class StorageService:
         self.conn.close()
 
     def execute(self, query):
+        print(query)
         self.cursor.execute(query)
         self.conn.commit()
 
@@ -25,22 +28,23 @@ class StorageService:
         self.cursor.execute(query)
         return self.cursor.fetchone()
 
-    def create_table(self, table_name, columns):
-        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
+    def create_table(self, table_name, columns = []):
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({SEP.join(columns)})"
         self.execute(query)
 
-    def insert(self, table_name, columns, values):
-        query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
+    def insert(self, table_name, columns = [], values = []):
+        query = f"INSERT INTO {table_name} ({SEP.join(columns)}) VALUES ({SEP.join(values)})"
         self.execute(query)
-
-    def update(self, table_name, columns, values, condition):
-        query = f"UPDATE {table_name} SET {columns} = {values} WHERE {condition}"
+        return self.cursor.lastrowid
+    
+    def update(self, table_name, columns = [], values = [], condition = ""):
+        query = f"UPDATE {table_name} SET {SEP.join(columns)} = {SEP.join(values)} WHERE {condition}"
         self.execute(query)
 
     def delete(self, table_name, condition):
         query = f"DELETE FROM {table_name} WHERE {condition}"
         self.execute(query)
 
-    def select(self, table_name, columns, condition):
-        query = f"SELECT {columns} FROM {table_name} WHERE {condition}"
+    def select(self, table_name, columns = [], condition = ""):
+        query = f"SELECT {SEP.join(columns)} FROM {table_name} WHERE {condition}"
         return self.fetchall(query)
