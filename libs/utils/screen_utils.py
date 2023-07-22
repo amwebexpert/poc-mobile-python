91f76f843e6c4f68065fb2b4b3d62c8de528c4a7
AndreTheViking <andre.masson@norda.com>
2023-07-22 12:37:29 -0400

    feat: typing for screen and string utils

--- libs/utils/screen_utils.py
+++ libs/utils/screen_utils.py
@@ -1,21 +1,21 @@
 from kivy.core.window import Window
 from libs.utils.platform_utils import is_ios, is_android
 
-def init_screen():
+def init_screen() -> None:
     if is_mobile():
         Window.maximize()
     else:
         Window.size = (800, 600)
 
-def get_screen_size():
+def get_screen_size() -> tuple:
     return Window.size
 
-def get_screen_width():
+def get_screen_width() -> int:
     return Window.width
 
-def get_screen_height():
+def get_screen_height() -> int:
     return Window.height
 
-def is_mobile():
+def is_mobile() -> bool:
     value = is_android() or is_ios()
     return value # To ease testing is_mobile() True on desktop, just return: not value
--- libs/utils/string_utils.py
+++ libs/utils/string_utils.py
@@ -1,7 +1,7 @@
-def is_blank(myString=None):
+def is_blank(myString = None) -> bool:
     if myString is None:
         return True
     return not myString.strip()
 
-def is_not_blank (myString=None):
-    return not isBlank(myString)
+def is_not_blank(myString = None) -> bool:
+    return not is_blank(myString)
