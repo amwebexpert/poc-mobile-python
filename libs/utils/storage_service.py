f74db7d9dd6c9bef85d8ce53424ee6dd71fb7de9
AndreTheViking <andre.masson@norda.com>
2023-07-22 12:39:56 -0400

    feat: add exception handling and typing to storage_service

--- libs/utils/storage_service.py
+++ libs/utils/storage_service.py
@@ -1,50 +1,76 @@
+from typing import List
 import sqlite3
+
+from libs.utils.string_utils import is_blank
 
 SEP = ", "
 
 class StorageService:
-    def __init__(self, db_name):
+    def __init__(self, db_name: str) -> None:
         self.db_name = db_name
         self.conn = None
         self.cursor = None
 
-    def connect(self):
+    def connect(self) -> None:
         self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
 
-    def close(self):
+    def close(self) -> None:
         self.conn.close()
-
-    def execute(self, query):
+    
+    def execute(self, query: str) -> None:
         print(f"--> sqlite: {query}")
-        self.cursor.execute(query)
-        self.conn.commit()
+        try:
+            self.cursor.execute(query)
+            self.conn.commit()
+        except Exception as e:
+            self.raise_exception(query, e)
+            self.db.rollback()    
 
-    def fetchall(self, query):
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
+    def fetchall(self, query: str) -> List[tuple]:
+        try:
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
+        except Exception as e:
+            self.raise_exception(query, e)
 
-    def fetchone(self, query):
-        self.cursor.execute(query)
-        return self.cursor.fetchone()
+    def fetchone(self, query: str) -> tuple:
+        try:
+            self.cursor.execute(query)
+            return self.cursor.fetchone()
+        except Exception as e:
+            self.raise_exception(query, e)
 
-    def create_table(self, table_name, columns = []):
+    def create_table(self, table_name: str, columns: List[str]) -> None:
+        if not columns:
+            raise ValueError("Must provide at least one column")
         query = f"CREATE TABLE IF NOT EXISTS {table_name} ({SEP.join(columns)})"
         self.execute(query)
 
-    def insert(self, table_name, columns = [], values = []):
+    def insert(self, table_name: str, columns: List[str], values: List[str]) -> int:
+        self.validateArrayLengths(columns, values)
         query = f"INSERT INTO {table_name} ({SEP.join(columns)}) VALUES ({SEP.join(values)})"
         self.execute(query)
         return self.cursor.lastrowid
     
-    def update(self, table_name, columns = [], values = [], condition = ""):
+    def update(self, table_name: str, columns: List[str], values: List[str], condition: str) -> None:
+        self.validateArrayLengths(columns, values)
         query = f"UPDATE {table_name} SET {SEP.join(columns)} = {SEP.join(values)} WHERE {condition}"
         self.execute(query)
 
-    def delete(self, table_name, condition):
+    def delete(self, table_name: str, condition: str) -> None:
         query = f"DELETE FROM {table_name} WHERE {condition}"
         self.execute(query)
+        if self.cursor.rowcount == 0:
+            print(f'No rows deleted for query: "{query}"')
 
-    def select(self, table_name, columns = [], condition = ""):
+    def select(self, table_name, columns: List[str], condition: str = "1=1"):
         query = f"SELECT {SEP.join(columns)} FROM {table_name} WHERE {condition}"
         return self.fetchall(query)
+
+    def raise_exception(self, query: str, e: Exception) -> None:
+        raise Exception(f'Error while executing query "{query}": {e}')
+
+    def validateArrayLengths(self, columns: List[str], values: List[str]):
+        if len(columns) != len(values):
+            raise ValueError(f"columns count: {len(columns)} != values count: {len(values)}")
