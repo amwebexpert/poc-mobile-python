218bd24e5175d54d227632c9a1462d7344a5d873
AndreTheFreeVicking <amwebexpert@gmail.com>
2023-08-27 11:01:32 -0300

    refactor: simplify animated robot icons mechanism

--- libs/theme/theme.py
+++ libs/theme/theme.py
@@ -28,27 +28,22 @@ class AnimatedIcons:
     widget: Widget
 
     def __init__(self) -> None:
-        self.widget = Factory.AnimatedIconButton()
+        self.widget = Factory.AnimatedRobotIcons()
 
-        row = self.widget.ids.icons_row
         for icon in ICONS:
-            row.add_widget(Factory.MDIconButton(
-                icon=icon, padding=(0, 0, 0, 0), icon_size="16sp"))
+            self.widget.add_widget(Factory.AminatedRobotIcon(icon=icon))
 
-        self.animation = Animation(opacity=0, d=0.7) + \
-            Animation(opacity=1, d=0.7)
+        self.animation = Animation(opacity=0, d=0.7) + Animation(opacity=1, d=0.7)
         self.animation.repeat = True
 
     def start_animation(self):
-        elements = self.widget.ids.icons_row.children
-        for index, item in enumerate(elements):
+        for index, item in enumerate(self.widget.children):
             self.start_animation_with_delay(item, index)
 
     def start_animation_with_delay(self, item: Widget, index: int):
         Clock.schedule_once(
-            lambda *args: self.animation.start(item), round(0.5 * index, 1))
+            lambda _: self.animation.start(item), round(0.5 * index, 1))
 
     def stop_animation(self):
-        elements = self.widget.ids.icons_row.children
-        for item in elements:
+        for item in self.widget.children:
             self.animation.stop(item)
